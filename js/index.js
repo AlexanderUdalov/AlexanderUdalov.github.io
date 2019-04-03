5aeef59f61036e7a2987512fd37dd1a58af1721b
AlexanderUdalov <alexudalovvvvv@outlook.com>
2019-04-03 18:45:06 +0300

    Add files via upload
    Init commit.

---
+++ js/index.js
@@ -0,0 +1,45 @@
+'use strict';
+
+$(document).ready(function () {
+	var $button = $('button[type="submit"]');
+
+	$button.on('click', function () {
+
+		var $this = $(this);
+		if ($this.hasClass('active') || $this.hasClass('success')) {
+			return false;
+		}
+		$this.addClass('active');
+		setTimeout(function () {
+			$this.addClass('loader');
+		}, 125);
+
+		var xhr = new XMLHttpRequest();
+        var url = 'http://localhost:9020/api/unityservice/';
+        xhr.open('POST', url, true);
+        xhr.onreadystatechange = function() { 
+           if (xhr.readyState != 4) return;
+           if (xhr.status != 200) {
+               	$this.removeClass('loader active');
+				$this.text('Ошибка');
+				$this.addClass('failed animated pulse');
+				setTimeout(function () {
+					$this.text('Запустить Unity');
+					$this.removeClass('failed animated pulse');
+					$this.blur();
+				}, 2900);
+           } else {
+               	$this.removeClass('loader active');
+				$this.text('Успешно');
+				$this.addClass('success animated pulse');
+				setTimeout(function () {
+					$this.text('Запустить Unity');
+					$this.removeClass('success animated pulse');
+					$this.blur();
+				}, 2900);
+           }
+        }
+
+       xhr.send("Vrap");
+	});
+});
